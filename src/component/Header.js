da5f1967eb0de2bec02a1221c3d4f3d9f5a6297a
javariyafatima1 <hoorfatima2729@gmail.com>
2025-01-24 22:27:34 +0500

    header responsive

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,61 +11,66 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { CartContext } from '../context/Context';
 import { Link } from 'react-router-dom';
-const Header = ({item}) => {
-   const {state:{cart},
-  } = CartContext();
-   return (
-    <Navbar  data-bs-theme="dark" id="n">
-        <Container>
-          <Navbar.Brand href="">Ecommerse</Navbar.Brand>
-          <Nav className="me-auto pt-3 mx-5 fs-5 ">
-            <Nav.Link className=" text-white"href="/">Home</Nav.Link>
-            <Nav.Link  className=" text-white"href="/about"> AboutUs</Nav.Link>
-          
-            <Nav.Link className=" text-white"href="">ContactUs</Nav.Link>
-            <Nav.Link className=" text-white"href="/card">Card</Nav.Link>
-             <input className="serchinput" type="text"  placeholder=" Search Ads Cars, mobile phone and More...."/>
-            <div className="search-line">
-            <FiSearch/>
-            </div>
-            <div className='drop'>
-            <Dropdown as={ButtonGroup}>
-      <Button variant="light" > <Nav.Link  className=" text-dark" href="/card" >   <FontAwesomeIcon icon={faShoppingCart} />{cart.length}
-      
-      </Nav.Link></Button>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
 
-      <Dropdown.Toggle split variant="dark" id="dropdown-split-basic" />
+const Header = () => {
+  const { state: { cart } } = CartContext();
 
-      <Dropdown.Menu >
-       {cart.length > 0 ?(
-        <>
-       {
-        cart.map(item =>(
-          <div className='side'>
-           <img src={item.image}/>
-            </div>
-        ))}
-        <Link to="/card">
-            <Button>
-              Show card
-            </Button>
-        </Link>
-        </>
-       ):
-       (
-        <span>cart is empty</span>
-       )
-      }
+  return (
+    <Navbar  variant="dark" expand="lg" className="py-3" id="n">
+      <Container fluid>
+        <Navbar.Brand href="/" className="fw-bold fs-4">Ecommerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto fs-5">
+            <Nav.Link className="text-white" href="/">Home</Nav.Link>
+            <Nav.Link className="text-white" href="/about">About Us</Nav.Link>
+            <Nav.Link className="text-white" href="/contact">Contact Us</Nav.Link>
+            <Nav.Link className="text-white" href="/card">Card</Nav.Link>
+          </Nav>
         
-      </Dropdown.Menu>
-    </Dropdown>
-    </div>
-            </Nav>
-        </Container>
-       
-      </Navbar>
-    
+          <div className="d-none d-md-flex align-items-center search-container">
+            <input
+              className="form-control me-2 search-input"
+              type="text"
+              placeholder="Search Ads, Cars, Mobile Phones, and More..."
+            />
+            <Button variant="light" className="search-btn">
+              <FiSearch />
+            </Button>
+          </div>
+          <Dropdown as={ButtonGroup} className="ms-3">
+            <Button variant="light">
+              <Nav.Link className="text-dark" href="/card">
+                <FontAwesomeIcon icon={faShoppingCart} /> {cart.length}
+              </Nav.Link>
+            </Button>
+            <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
+            <Dropdown.Menu>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((item, index) => (
+                    <div className="dropdown-item d-flex align-items-center" key={index}>
+                      <img src={item.image} alt="cart-item" className="cart-item-img me-2" />
+                      <span>{item.name}</span>
+                    </div>
+                  ))}
+                  <Link to="/card">
+                    <Button variant="primary" className="mt-2 w-100">
+                      Show Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span className="dropdown-item">Cart is empty</span>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-}
+};
 
 export default Header;
